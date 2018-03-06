f9c3602e0247f4108a30bea62339e488eb18a936
maus <maksimochka200@gmail.com>
2018-03-06 15:35:55 +0300

    changed variables names and add class for table

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -16,22 +16,22 @@ public class Controller {
     public Button button51;
     public Button button52;
     public Button button53;
-    public TextField text_field1;
-    public TextField text_field2;
-    public TextField text_field3;
-    public TextField text_field4;
-    public TextField text_field5;
+    public TextField textField1;
+    public TextField textField2;
+    public TextField textField3;
+    public TextField textField4;
+    public TextField textField5;
     public ComboBox<String> list;
-    public RadioButton radio_button_1;
-    public RadioButton radio_button_2;
-    public RadioButton radio_button_3;
-    public CheckBox check_box_1;
-    public CheckBox check_box_2;
-    public CheckBox check_box_3;
-    public TableView<table_5_data> table_5;
+    public RadioButton radioButton1;
+    public RadioButton radioButton2;
+    public RadioButton radioButton3;
+    public CheckBox checkBox1;
+    public CheckBox checkBox2;
+    public CheckBox checkBox3;
+    public TableView<tableData5> table5;
     public TableColumn<String, String> col1;
     public TableColumn<String, String> col2;
-    private final ObservableList<table_5_data> data = FXCollections.observableArrayList();
+    private final ObservableList<tableData5> data = FXCollections.observableArrayList();
 
     private void alert(String text){
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -43,127 +43,105 @@ public class Controller {
         alert.showAndWait();
     }
 
-    public void press_but1(ActionEvent actionEvent) {
-        if (text_field1.getText().equals("")){
+    public void pressBut1(ActionEvent actionEvent) {
+        if (textField1.getText().equals("")){
            alert("Please, type some text");
         }
         else {
-            if (list.getItems().contains(text_field1.getText())) {
+            if (list.getItems().contains(textField1.getText())) {
                 alert("You cannot add this text. It's already added");
             } else {
-                list.getItems().add(text_field1.getText());
+                list.getItems().add(textField1.getText());
             }
         }
     }
 
-    public void press_but52(ActionEvent actionEvent) {
-        if (table_5.getSelectionModel().getSelectedItems() == null) {
+    public void pressBut52(ActionEvent actionEvent) {
+        if (table5.getSelectionModel().getSelectedItems() == null) {
             alert("Please, select any row on table");
         } else {
-            ObservableList<table_5_data> selected_item = table_5.getSelectionModel().getSelectedItems();
-            if (!selected_item.get(0).getFirst_list().equals("")){
-                data.set(data.indexOf(selected_item.get(0)), new table_5_data("", selected_item.get(0).getFirst_list()));
+            ObservableList<tableData5> selected_item = table5.getSelectionModel().getSelectedItems();
+            if (!selected_item.get(0).getFirstList().equals("")){
+                data.set(data.indexOf(selected_item.get(0)), new tableData5("", selected_item.get(0).getFirstList()));
             }
         }
     }
 
-    public void press_but53(ActionEvent actionEvent) {
-        if (table_5.getSelectionModel().getSelectedItems() == null) {
+    public void pressBut53(ActionEvent actionEvent) {
+        if (table5.getSelectionModel().getSelectedItems() == null) {
             alert("Please, select any row on table");
         } else {
-            ObservableList<table_5_data> selected_item = table_5.getSelectionModel().getSelectedItems();
-            if (!selected_item.get(0).getSecond_list().equals("")){
-                data.set(data.indexOf(selected_item.get(0)), new table_5_data(selected_item.get(0).getSecond_list(), ""));
+            ObservableList<tableData5> selected_item = table5.getSelectionModel().getSelectedItems();
+            if (!selected_item.get(0).getSecondList().equals("")){
+                data.set(data.indexOf(selected_item.get(0)), new tableData5(selected_item.get(0).getSecondList(), ""));
             }
         }
     }
 
-    public class table_5_data{
-        private String first_list;
-        private String second_list;
-
-        table_5_data(String first_list, String second_list){
-            this.first_list = first_list;
-            this.second_list = second_list;
-        }
-
-        public String getFirst_list() {
-            return first_list;
-        }
-
-        public String getSecond_list() {
-            return second_list;
-        }
-    }
-
-    public void press_but51(ActionEvent actionEvent) {
-        if (text_field5.getText().equals("")){
+    public void pressBut51(ActionEvent actionEvent) {
+        if (textField5.getText().equals("")){
             alert("Please, enter any text");
         }
         else {
-            col1.setCellValueFactory(new PropertyValueFactory<String, String>("first_list"));
-            col2.setCellValueFactory(new PropertyValueFactory<String, String>("second_list"));
-            String first_list = "";
-            String second_list = "";
-            first_list = "ASD";
-            second_list = "FRE";
-            data.add(new table_5_data(text_field5.getText(), ""));
-            table_5.setItems(data);
+            col1.setCellValueFactory(new PropertyValueFactory<>("firstList"));
+            col2.setCellValueFactory(new PropertyValueFactory<>("secondList"));
+            data.add(new tableData5(textField5.getText(), ""));
+            table5.setItems(data);
         }
     }
 
-    public void press_but4(ActionEvent actionEvent) {
-        if (text_field4.getText().equals(check_box_1.getText())){
-            check_box_1.setSelected(true);
-            check_box_2.setSelected(false);
-            check_box_3.setSelected(false);
+    public void pressBut4(ActionEvent actionEvent) {
+        if (textField4.getText().equals(checkBox1.getText())){
+            checkBox1.setSelected(true);
+            checkBox2.setSelected(false);
+            checkBox3.setSelected(false);
         }
-        else if (text_field4.getText().equals(check_box_2.getText())){
-            check_box_1.setSelected(false);
-            check_box_2.setSelected(true);
-            check_box_3.setSelected(false);
+        else if (textField4.getText().equals(checkBox2.getText())){
+            checkBox1.setSelected(false);
+            checkBox2.setSelected(true);
+            checkBox3.setSelected(false);
         }
-        else if (text_field4.getText().equals(check_box_3.getText())){
-            check_box_1.setSelected(false);
-            check_box_2.setSelected(false);
-            check_box_3.setSelected(true);
+        else if (textField4.getText().equals(checkBox3.getText())){
+            checkBox1.setSelected(false);
+            checkBox2.setSelected(false);
+            checkBox3.setSelected(true);
         }
         else {
             alert("This text is not equals to any box");
         }
     }
 
-    public void press_but3(ActionEvent actionEvent) {
-        if (text_field3.getText().equals(radio_button_1.getText())){
-            radio_button_1.setSelected(true);
-            radio_button_2.setSelected(false);
-            radio_button_3.setSelected(false);
+    public void pressBut3(ActionEvent actionEvent) {
+        if (textField3.getText().equals(radioButton1.getText())){
+            radioButton1.setSelected(true);
+            radioButton2.setSelected(false);
+            radioButton3.setSelected(false);
         }
-        else if (text_field3.getText().equals(radio_button_2.getText())){
-            radio_button_1.setSelected(false);
-            radio_button_2.setSelected(true);
-            radio_button_3.setSelected(false);
+        else if (textField3.getText().equals(radioButton2.getText())){
+            radioButton1.setSelected(false);
+            radioButton2.setSelected(true);
+            radioButton3.setSelected(false);
         }
-        else if (text_field3.getText().equals(radio_button_3.getText())){
-            radio_button_1.setSelected(false);
-            radio_button_2.setSelected(false);
-            radio_button_3.setSelected(true);
+        else if (textField3.getText().equals(radioButton3.getText())){
+            radioButton1.setSelected(false);
+            radioButton2.setSelected(false);
+            radioButton3.setSelected(true);
         }
         else {
             alert("This text is not equals to any button");
         }
     }
 
-    public void press_but21(ActionEvent actionEvent) {
-        if (text_field2.getText().equals("")){
+    public void pressBut21(ActionEvent actionEvent) {
+        if (textField2.getText().equals("")){
             alert("Please, type some text");
         }
         else{
-            button22.setText(text_field2.getText());
+            button22.setText(textField2.getText());
         }
 
     }
-    public void press_but22(ActionEvent actionEvent) {
+    public void pressBut22(ActionEvent actionEvent) {
             String var = button21.getText();
             button21.setText(button22.getText());
             button22.setText(var);
---
+++ src/sample/tableData5.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class tableData5 {
+    private String firstList;
+    private String secondList;
+
+    tableData5(String first_list, String second_list){
+        this.firstList = first_list;
+        this.secondList = second_list;
+    }
+
+    public String getFirstList() {
+        return firstList;
+    }
+
+    public String getSecondList() {
+        return secondList;
+    }
+}
